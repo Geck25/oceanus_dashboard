fa1ffb291a9e07fbf4f6a9aa1234dced35c22027
SecondarySkyler <skylerhunter6@gmail.com>
2022-11-16 16:18:21 +0100

    added modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,9 +9,14 @@ import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { WidgetComponent } from './widget/widget.component';
 import { TabbarComponent } from './tabbar/tabbar.component';
+import { PanelDialogComponent } from './panel-dialog/panel-dialog.component';
 
 /** Angular Material Imports */
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -18,13 +24,22 @@ import { MatTabsModule } from '@angular/material/tabs';
     DashboardComponent,
     WidgetComponent,
     TabbarComponent,
+    PanelDialogComponent,
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
     NoopAnimationsModule,
-    MatTabsModule
+    MatTabsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatDialogModule,
+    MatInputModule
+  ],
+  exports: [
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
