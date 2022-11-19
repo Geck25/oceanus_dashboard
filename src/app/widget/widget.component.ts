6bd1101eb779ba9f94cd030f5f87d93d3049bb66
cristian <skylerhunter6@gmail.com>
2022-11-19 10:38:40 +0100

    added fullname/u.o.m. plus some style

--- src/app/widget/widget.component.ts
+++ src/app/widget/widget.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input, Inject, SimpleChanges, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
+import { measures } from '../utils/measure';
 
 @Component({
   selector: 'app-widget',
@@ -8,10 +9,14 @@ import { Component, OnInit, Input, Inject, SimpleChanges, OnChanges } from '@ang
 export class WidgetComponent implements OnInit, OnChanges {
   @Input() measureName: any;
   @Input() measureValue: any;
+  measureFullName: string = '';
+  unitOfMeasurement: string = '';
 
   constructor() {}
 
   ngOnInit(): void {
+    this.measureFullName = measures[this.measureName].fullName;
+    this.unitOfMeasurement = measures[this.measureName].unitOfMeasurement;
   }
 
   ngOnChanges(changes: SimpleChanges): void{
