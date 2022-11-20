42b82a20a7b43b8c88e534207ec2182a6ec4bf51
cristian <skylerhunter6@gmail.com>
2022-11-20 09:32:54 +0100

    added measurement from navi/gps/target/wind panel

--- src/app/utils/measure.ts
+++ src/app/utils/measure.ts
@@ -4,6 +4,17 @@ interface measureInfo {
 }
 
 export const measures: Record<string, measureInfo> = {
-    aws: {fullName: 'Apparent Wind Speed', unitOfMeasurement: 'kn'},
-    awa: {fullName: 'Apparent Wind Angle', unitOfMeasurement: '°'}
+    aws: {fullName: 'Apparent Wind Speed', unitOfMeasurement: 'kt'},
+    awa: {fullName: 'Apparent Wind Angle', unitOfMeasurement: '°'},
+    sow: {fullName: 'Speed Over Water', unitOfMeasurement: 'kt'},
+    mh: {fullName: 'Magnetic Heading', unitOfMeasurement: '°'},
+    cog: {fullName: 'Course Over Ground', unitOfMeasurement: '°'},
+    sog: {fullName: 'Speed Over Ground', unitOfMeasurement: 'kt'},
+    delta_vmg: {fullName: 'Delta VMG', unitOfMeasurement: 'kt'},
+    delta_twa: {fullName: 'Delta Angle', unitOfMeasurement: '°'},
+    delta_vpp: {fullName: 'Delta VPP', unitOfMeasurement: 'kt'},
+    vmg: {fullName: 'Vmg', unitOfMeasurement: 'kt'},
+    tws: {fullName: 'True Wind Speed', unitOfMeasurement: 'kt'},
+    twa: {fullName: 'True Wind Angle', unitOfMeasurement: '°'},
+
 }
