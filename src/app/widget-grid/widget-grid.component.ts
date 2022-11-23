4f18cc2e8eb1c0a674eb4c2e9f83c7b8d82469b7
cristian <skylerhunter6@gmail.com>
2022-11-23 14:43:56 +0100

    a grid made with MatChip, used in "add panel" dialog

---
+++ src/app/widget-grid/widget-grid.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { MatChip } from '@angular/material/chips';
+import { TelemetryService } from '../telemetry.service';
+
+@Component({
+  selector: 'app-widget-grid',
+  templateUrl: './widget-grid.component.html',
+  styleUrls: ['./widget-grid.component.css']
+})
+export class WidgetGridComponent implements OnInit {
+  measures: string[] = [];
+  selectedMeasures: string[] = [];
+  error: string = '';
+  @Output() submitSelectedMeasuresEvent = new EventEmitter<MatChip>();
+
+  constructor(private telemetry: TelemetryService) { }
+
+  ngOnInit(): void {
+    this.telemetry.getTelemetry().subscribe(data => {
+      this.measures = Object.keys(data);
+    })
+  }
+
+  sendNewItemToParent(chip: MatChip) {
+    
+    this.submitSelectedMeasuresEvent.emit(chip);
+  }
+
+}
