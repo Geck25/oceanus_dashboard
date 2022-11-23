082e24d050950af729618b66596c9806d5b63096
cristian <skylerhunter6@gmail.com>
2022-11-23 15:28:29 +0100

    added latitude - longitude - date

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { map, Subscription, timer } from 'rxjs';
+import { TelemetryService } from '../telemetry.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  private subscription: Subscription = Subscription.EMPTY;
+  response: any = null;
+  private coordinates: string[] = ['lat', 'lon'];
+  currentDate: Date = new Date();
+  lat: string = '';
+  lon: string = '';
 
-  constructor() { }
+  constructor(private telemetry: TelemetryService) {
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
+  }
 
   ngOnInit(): void {
+    this.subscription = timer(0, 2000).pipe(
+      map(() => {
+        this.telemetry.getTelemetry().subscribe(data => {
+          this.response = data; // Object
+          const asArray = Object.entries(data);
+          const filtered = asArray.filter(([key, value]) => this.coordinates.includes(key));
+          this.response = Object.fromEntries(filtered);
+          this.lat = this.response['lat'];
+          this.lon = this.response['lon'];
+        });
+      })
+    ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
 }
