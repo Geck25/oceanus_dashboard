f8dedfcca10c614030e042c6c9e408862f003d48
cristian <skylerhunter6@gmail.com>
2022-11-23 14:53:23 +0100

    routing for HomeComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, ROUTES, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
 import { ConfigService } from './services/config.service';
 
 const routes: Routes = [
-  //{ path: '', redirectTo: 'dashboard/gps', pathMatch: 'full'},
-  { path: 'dashboard/:dashboard-name', component: DashboardComponent}
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dashboard/:dashboard-name', component: DashboardComponent }
 ];
 
 @NgModule({
