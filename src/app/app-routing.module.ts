b0bc1634fa841f849cdba6eda91d5232d7b8f750
cristian <skylerhunter6@gmail.com>
2022-11-23 14:39:21 +0100

    routing for panels

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, ROUTES, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ConfigService } from './services/config.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: '', redirectTo: 'dashboard/gps', pathMatch: 'full'},
+  { path: 'dashboard/:dashboard-name', component: DashboardComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
