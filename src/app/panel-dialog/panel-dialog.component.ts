27ac5e41d4de67be1b9326040f52d9a06ccab7d3
cristian <skylerhunter6@gmail.com>
2022-11-23 14:46:40 +0100

    added logic for measures selection

--- src/app/panel-dialog/panel-dialog.component.ts
+++ src/app/panel-dialog/panel-dialog.component.ts
@@ -1,7 +1,9 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { MatChip } from '@angular/material/chips';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ConfigService } from '../services/config.service';
+import { measures } from '../utils/measure';
 
 @Component({
   selector: 'app-panel-dialog',
@@ -10,8 +12,9 @@ import { ConfigService } from '../services/config.service';
 })
 export class PanelDialogComponent implements OnInit {
   panelName = new FormControl<string>('', {nonNullable: true});
-  selectedMeasure = new FormControl<string>('', {nonNullable: true});
   config: any = null;
+  selectedMeasures: string[] = [];
+  error: string = '';
 
   constructor(
     public dialogRef: MatDialogRef<PanelDialogComponent>,
@@ -29,20 +32,42 @@ export class PanelDialogComponent implements OnInit {
   addPanel(): void {
     var currentConfig = this.configService.getConfig();
     if (currentConfig === null) {
-      // devo costruire il json 
-      let cfg: string = "{\"" + this.panelName.value + "\": [\"" + this.selectedMeasure.value + "\"] }";
-      this.configService.saveConfig(cfg);
+      let cfgAsString: string =`{"${this.panelName.value}" : [`;
+      for (let measure of this.selectedMeasures) {
+        cfgAsString += `\"${measure}\", `;
+      }
+      cfgAsString += "]}";
+      // regex per rimuovere ', ' alla fine della string
+      cfgAsString = cfgAsString.replace(/,\s(?=[^,]*$)/, "");
+      console.log(cfgAsString);
+      this.configService.saveConfig(cfgAsString);
     } else {
       // se c'è già una configurazione faccio il parsing e aggiungo la nuova tab
       this.config = JSON.parse(currentConfig);
-      this.config[this.panelName.value] = [this.selectedMeasure.value];
+      this.config[this.panelName.value] = this.selectedMeasures;
       let newCfg = JSON.stringify(this.config);
       this.configService.saveConfig(newCfg);
-
     }
     this.dialogRef.close();
     // permette di ricaricare la pagina e quindi di aggiornare la tab bar
     window.location.reload();
+  }
+
+  addMeasure(newChip: MatChip) {
+    if (newChip.selected) {
+      const index = this.selectedMeasures.indexOf(newChip.value);
+      this.selectedMeasures.splice(index, 1);
+      newChip.toggleSelected();
+      this.error = '';
+    } else {
+      if (this.selectedMeasures.length + 1 <= 4) {
+        this.selectedMeasures.push(newChip.value);
+        newChip.toggleSelected();
+      } else {
+        this.error = 'Puoi avere massimo 4 widgets!'
+      }
+    }
+    console.log(this.selectedMeasures);
   }
 
 }
