06a36d03163dede7b37c7d3f2c7987c22e79e311
cristian <skylerhunter6@gmail.com>
2022-11-26 15:33:44 +0100

    added comment and dialog validation on submit

--- src/app/panel-dialog/panel-dialog.component.ts
+++ src/app/panel-dialog/panel-dialog.component.ts
@@ -1,5 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { MatChip } from '@angular/material/chips';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ConfigService } from '../services/config.service';
@@ -11,10 +11,11 @@ import { measures } from '../utils/measure';
   styleUrls: ['./panel-dialog.component.css']
 })
 export class PanelDialogComponent implements OnInit {
-  panelName = new FormControl<string>('', {nonNullable: true});
+  panelName = new FormControl<string>('', Validators.required);
   config: any = null;
   selectedMeasures: string[] = [];
-  error: string = '';
+  someError: boolean = false;
+  widgetsOverflowError: string = '';
 
   constructor(
     public dialogRef: MatDialogRef<PanelDialogComponent>,
@@ -29,28 +30,36 @@ export class PanelDialogComponent implements OnInit {
     this.dialogRef.close();
   }
 
+  /**
+   * Funzione che viene eseguita al 'submit' del bottone presente nel dialog
+   * utilizza il ConfigService per recuperare, se presente, l'attuale configurazione 
+   * salvata nel local storage
+   */
   addPanel(): void {
-    var currentConfig = this.configService.getConfig();
-    if (currentConfig === null) {
-      let cfgAsString: string =`{"${this.panelName.value}" : [`;
-      for (let measure of this.selectedMeasures) {
-        cfgAsString += `\"${measure}\", `;
+    if (this.panelName.valid && this.selectedMeasures.length === 4) {
+      var currentConfig = this.configService.getConfig();
+      if (currentConfig === null) {
+        let cfgAsString: string =`{"${this.panelName.value}" : [`;
+        for (let measure of this.selectedMeasures) {
+          cfgAsString += `\"${measure}\", `;
+        }
+        cfgAsString += "]}";
+        // regex per rimuovere ', ' alla fine della stringa
+        cfgAsString = cfgAsString.replace(/,\s(?=[^,]*$)/, "");
+        this.configService.saveConfig(cfgAsString);
+      } else {
+        // se c'è già una configurazione faccio il parsing e aggiungo la nuova tab
+        this.config = JSON.parse(currentConfig);
+        this.config[this.panelName.value!] = this.selectedMeasures;
+        let newCfg = JSON.stringify(this.config);
+        this.configService.saveConfig(newCfg);
       }
-      cfgAsString += "]}";
-      // regex per rimuovere ', ' alla fine della string
-      cfgAsString = cfgAsString.replace(/,\s(?=[^,]*$)/, "");
-      console.log(cfgAsString);
-      this.configService.saveConfig(cfgAsString);
+      this.dialogRef.close();
+      // permette di ricaricare la pagina e quindi di aggiornare la tab bar
+      window.location.reload();
     } else {
-      // se c'è già una configurazione faccio il parsing e aggiungo la nuova tab
-      this.config = JSON.parse(currentConfig);
-      this.config[this.panelName.value] = this.selectedMeasures;
-      let newCfg = JSON.stringify(this.config);
-      this.configService.saveConfig(newCfg);
+      this.someError = true;
     }
-    this.dialogRef.close();
-    // permette di ricaricare la pagina e quindi di aggiornare la tab bar
-    window.location.reload();
   }
 
   addMeasure(newChip: MatChip) {
@@ -58,13 +67,13 @@ export class PanelDialogComponent implements OnInit {
       const index = this.selectedMeasures.indexOf(newChip.value);
       this.selectedMeasures.splice(index, 1);
       newChip.toggleSelected();
-      this.error = '';
+      this.widgetsOverflowError = '';
     } else {
       if (this.selectedMeasures.length + 1 <= 4) {
         this.selectedMeasures.push(newChip.value);
         newChip.toggleSelected();
       } else {
-        this.error = 'Puoi avere massimo 4 widgets!'
+        this.widgetsOverflowError = 'Puoi avere massimo 4 widgets!'
       }
     }
     console.log(this.selectedMeasures);
