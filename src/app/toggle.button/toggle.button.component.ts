0262b8d9bba53e88d526a41ff9ad5fc879eca46f
SecondarySkyler <skylerhunter6@gmail.com>
2022-12-06 21:13:21 +0100

    Created a toggle button component

---
+++ src/app/toggle.button/toggle.button.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { TabbarComponent } from '../tabbar/tabbar.component';
+
+@Component({
+  selector: 'toggle-button',
+  templateUrl: './toggle.button.component.html',
+  styleUrls: ['./toggle.button.component.css']
+})
+export class ToggleButtonComponent {
+  @Input() isSelected: boolean = false;
+  @Input() isHomeEndPoint: boolean = false;
+  @Input() endPoint: string = '';
+
+  constructor(private container: TabbarComponent) { }
+
+    // change the state of the container
+    toggle() {
+      this.container.toggleButton(this)
+    }
+  
+    toggleSelection() {
+      this.isSelected = !this.isSelected;
+    }
+
+}
