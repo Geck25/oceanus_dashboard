63a173cca95329e7d85cc7cc4b92e69bf822e6aa
cristian <skylerhunter6@gmail.com>
2022-12-14 15:33:54 +0100

    added routing for regata field component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, ROUTES, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { RegataFieldComponent } from './regata-field/regata-field.component';
 import { ConfigService } from './services/config.service';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'regata-field', component: RegataFieldComponent},
   { path: 'dashboard/:dashboard-name', component: DashboardComponent }
 ];
 
