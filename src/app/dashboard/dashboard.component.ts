44c6133e4ce4b3852e3bd7c964412e73dc83ee9d
cristian <skylerhunter6@gmail.com>
2022-12-16 14:44:02 +0100

    added a dialog to confirm the cancelation of a panel

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { BreakpointObserver } from '@angular/cdk/layout';
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { map, Subscription, timer } from 'rxjs';
 import { ConfigService } from '../services/config.service';
@@ -25,6 +25,7 @@ export class DashboardComponent implements OnInit {
   row: number = 2;
   isSmallDimension: boolean = false;
   panelName: string = '';
+  @ViewChild('confirmModal') confirmModal: ElementRef<HTMLElement>;
 
 
   constructor(
@@ -72,7 +73,23 @@ export class DashboardComponent implements OnInit {
   deletePanel(): void {
     delete this.config[this.panelName];
     this.configService.saveConfig(JSON.stringify(this.config));
+    this.closeConfirmDialog();
     this.router.navigate(['/home']).then(() => window.location.reload())
+  }
+
+  openConfirmDialog(): void {
+    this.confirmModal.nativeElement.style.display = 'flex';
+  }
+
+  closeConfirmDialog(): void {
+    this.confirmModal.nativeElement.style.display = 'none';
+  }
+
+  @HostListener('window:click', ['$event.target'])
+  onOutsideModalClick(target: HTMLElement) {
+    if (target === this.confirmModal.nativeElement) {
+      this.confirmModal.nativeElement.style.display = 'none';
+    }
   }
 
   isCompass(measure: any): boolean {
